a4ad8bdded8ac52993656106fb44640a420c61e5
Hasan Azizul Haque <hasanavi@gmail.com>
2018-03-16 02:00:43 +0000

    include foundation grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Row, Column } from 'react-foundation';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="grid-basics-example">
+          <Row className="App-intro">
+            <Column small={2} large={4}>4 columns</Column>
+            <Column small={4} large={4}>4 columns</Column>
+            <Column small={6} large={4}>4 columns</Column>
+          </Row>
+        </div>
       </div>
     );
   }
